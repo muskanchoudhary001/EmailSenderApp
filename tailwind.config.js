f680ab87765ce25a098d654ae5f52674bf5e2b41
Muskan Choudhary <81254978+muskanchoudhary001@users.noreply.github.com>
2025-07-22 09:27:04 +0530

    project structure setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        './views/**/*.{html,js}',  // Add paths to your views or HTML files
+        './public/**/*.{html,js}',  // Include any other HTML/JS files if needed
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
